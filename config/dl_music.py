558d373de729d7ed935bfcfffaa1e87114ab4fd7
vimman <kentin@ymail.com>
2018-02-17 01:59:46 +0100

    nice colors

--- config/dl_music.py
+++ config/dl_music.py
@@ -12,13 +12,13 @@
 
 #!/usr/bin/env python3
 
+# Apparently it work's with python so maybe no need to install python3
+
 import os
-os.system('pip3 install --upgrade pip')
-os.system('sudo -H pip3 install pyyaml')
 import yaml
 
-os.system('mkdir -p music')
 from yaml import load, dump
+from termcolor import colored
 with open("bd.yml", 'r') as stream:
     try:
         from yaml import CLoader as Loader, CDumper as Dumper
@@ -30,21 +30,13 @@ dict = {}
 for dirname, dirnames, filenames in os.walk('./music'):
     for filename in filenames:
         dict[filename] = filename
-        #print(filename)
 
 for key in data.keys():
     tmp = key + ".wav"
-    print(key)
+    print colored(key, 'yellow')
     if tmp in dict:
-        print("allready in my jukebox baby !")
+        print colored("allready in my jukebox baby !", 'green')
     else:
-        print("add music")
-        if os.system("which ffmpeg"):
-            print("installing ffmpeg")
-            os.system("sudo apt-get install ffmpeg")
-        if os.system("which youtube-dl"):
-            print("installing youtube-dl")
-            os.system("sudo pip install --upgrade youtube_dl")
-        os.system("youtube-dl -x "+data[key]['url']+" -o music/"+key+".opus")
-        os.system("ffmpeg -i music/"+key+".opus music/"+tmp+"")
-        os.system("rm music/"+key+".opus")
+        print colored("add music", 'blue')
+        os.system("youtube-dl --quiet -x --audio-quality 0 --audio-format 'wav' \
+        "+data[key]['url']+" -o music/"+key+".%\(ext\)s")
